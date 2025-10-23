86e7fcf858cffcc896db29aaa2439a39d6ef89b9
lahiruXONT <lahiru.d@xontworld.com>
2025-10-23 16:37:46 +0530

    changes

--- XONT.Ventura.ShellApp/XONT.Ventura.ShellApp/Controllers/SettingsController.cs
+++ XONT.Ventura.ShellApp/XONT.Ventura.ShellApp/Controllers/SettingsController.cs
@@ -128,7 +128,6 @@ namespace XONT.Ventura.ShellApp.Controllers
     {
         public string CurrentPassword { get; set; } = string.Empty;
         public string NewPassword { get; set; } = string.Empty;
-        public string ConfirmNewPassword { get; set; } = string.Empty;
     }
 
     public class ProfileImageRequest
